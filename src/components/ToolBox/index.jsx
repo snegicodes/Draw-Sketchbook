64fd546b804ea75a2036aa874c5ff8da374acce4
Shruti-2303 <sharmas23032001@gmail.com>
2023-12-22 21:00:28 +0530

    Feat: Added ToolBox Component

---
+++ src/components/ToolBox/index.jsx
@@ -0,0 +1,38 @@
+import styles from "./index.module.css";
+
+const ToolBox = () => {
+  return (
+    <div className={styles.toolboxContainer}>
+      <div className={styles.toolItem}>
+        <div className={styles.toolText}>Stroke Color</div>
+        <div className={styles.itemContainer}>
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: "black" }}
+          />
+          <div className={styles.colorBox} style={{ backgroundColor: "red" }} />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: "orange" }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: "green" }}
+          />
+          <div
+            className={styles.colorBox}
+            style={{ backgroundColor: "blue" }}
+          />
+        </div>
+      </div>
+      <div className={styles.toolItem}>
+        <div className={styles.toolText}>Brush Stroke</div>
+        <div className={styles.itemContainer}>
+          <input type="range" min={1} max={10} step={1} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToolBox;
