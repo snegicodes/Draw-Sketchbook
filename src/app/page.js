ca2883248717253fbfae3565c82d4e16bf4c51b2
SarthakNegi20 <sarthak.negi@leverageedu.com>
2023-12-23 23:00:52 +0530

    Feat: Added Redux toolkit Store

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
+"use client";
 import Menu from "@/components/Menu";
 import ToolBox from "@/components/ToolBox";
+import Board from "@/components/Board";
+import { Provider } from "react-redux";
+import { store } from "@/store";
 
 export default function Home() {
   return (
-    <div>
+    <Provider store={store}>
       <Menu />
       <ToolBox />
-    </div>
+      <Board />
+    </Provider>
   );
 }
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -6,13 +6,27 @@ import {
   FaFileDownload,
 } from "react-icons/fa";
 import styles from "./index.module.css";
+import { useDispatch } from "react-redux";
+import { MENU_ITEMS } from "@/constants";
+import { menuItemClick } from "@/slice/menuSlice";
 const Menu = () => {
+  const dispatch = useDispatch();
+
+  const handleMenuClick = (itemName) => {
+    dispatch(menuItemClick(itemName));
+  };
   return (
     <div className={styles.menuContainer}>
-      <div className={styles.iconWrapper}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}
+      >
         <FaPencilAlt className={styles.icon} />
       </div>
-      <div className={styles.iconWrapper}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}
+      >
         <FaEraser className={styles.icon} />
       </div>
       <div className={styles.iconWrapper}>
